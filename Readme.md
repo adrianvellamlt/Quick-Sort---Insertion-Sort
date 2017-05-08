848c5516ab44c5e265c9e52c66df7bcfb42319cc
Adrian Vella <adrian.vella@outlook.com>
2017-05-08 18:59:13 +0200

    Fix with saving of input and output files. Included new params

--- Readme.md
+++ Readme.md
@@ -9,5 +9,12 @@ Sort and Quick Sort according to the array size.
 
 - Algorithm name :: (QuickSort/ SortMedianOfThree/ OptimsedSort)
 - Size of array to be sorted :: (If you have a csv file already, write 0)
-- Input file name :: (If you have a csv file already, provide path and name here)
+- Unique list :: 0 = duplicate values, 1 = unique values
+- Shuffle :: 0 = not shuffled, 1 = slightly shuffled, 2 = shuffled thoroughly
+- Input file name :: (If you have a csv file already, provide name here. File has to be in dir)
 - Output file name
+
+### Update
+
+More parameters added. Support for 3 different shuffles and duplicates in list. 
+Fix for save path for input and output files.
--- utils.py
+++ utils.py
@@ -2,7 +2,8 @@ import time
 import numpy as np
 import hashlib
 import quicksort as qsort
-import sys
+import sys, os
+from random import randint
 qs = qsort.QuickSort()
 
 definitions = {
@@ -22,14 +23,14 @@ def TimedSortFunction(params):
     print('function [{}] finished in {} ms'.format(
         func.__name__, int(elapsedTime * 1000)))
 
-def NewArrayToCSV(size, isUnique = True):
-    arr = []
-    if not isUnique:
-        arr = list(np.random.choice(size//4, size, replace=True))
-    if isUnique:
-        arr = list(np.random.choice(size, size, replace=False))
-        set(arr)
+def NewArrayToCSV(size, isUnique, shuffle):
+    arr = list(np.random.choice(size, size, replace=not isUnique))
     print('Array Size ::', len(arr))
+    if shuffle is '0':
+        arr.sort()
+    elif shuffle is '1':
+        arr.sort()
+        arr = SlightlyShuffle(arr)
     np.savetxt(definitions['CSVFileName'], arr, fmt='%d', delimiter=',')
 
 def CSVToArray(path):
@@ -45,15 +46,21 @@ def ChooseAlgorithm(func):
 def InitialSetup():
     chosenAlgorithm = ''
     arrSize = 0
+    isUnique = 1
+    shuffle = 0
 
     if len(sys.argv) > 1:
         chosenAlgorithm = sys.argv[1]
         arrSize = sys.argv[2]
-        definitions['CSVFileName'] = sys.argv[3]
-        definitions['Output'] = sys.argv[4]
+        isUnique = sys.argv[3]
+        shuffle = sys.argv[4]
+        definitions['CSVFileName'] = sys.argv[5]
+        definitions['Output'] = sys.argv[6]
     else:
         chosenAlgorithm = input('Write the algorithm to be used: (QuickSort, SortMedianOfThree, OptimisedSort):\n')
         arrSize = input('Write the size of the array:\n')
+        isUnique = input('All unique elements? (0/1)\n')
+        shuffle = input('How shuffled should the list be? (0/1/2)\n')
         definitions['CSVFileName'] = input('Write input file name:\n')
         definitions['Output'] = input('Write output file name:\n')
 
@@ -61,10 +68,19 @@ def InitialSetup():
         definitions['CSVFileName'] = definitions['CSVFileName'] + '.csv'
     if not definitions['Output'].endswith('.csv'):
         definitions['Output'] = definitions['Output'] + '.csv'
+    definitions['CSVFileName'] = os.path.abspath(os.path.dirname(sys.argv[0])) + '/' + definitions['CSVFileName']
+    definitions['Output'] = os.path.abspath(os.path.dirname(sys.argv[0])) + '/' + definitions['Output']
 
     arr = []
     if(arrSize is not 0):
-        NewArrayToCSV(int(arrSize))
+        NewArrayToCSV(int(arrSize),True if isUnique is '1' else False, shuffle)
     arr = CSVToArray(definitions['CSVFileName'])
 
     return chosenAlgorithm, arr
+
+def SlightlyShuffle(arr):
+    for i in range(len(arr)//15):
+        randNo1 = randint(0, len(arr))
+        randNo2 = randint(0, len(arr))
+        arr[randNo1], arr[randNo2] = arr[randNo2], arr[randNo1]
+    return arr
